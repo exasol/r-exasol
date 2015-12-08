d065668363fc24be99ebfe399a12b1f94d98bc53
Viliam Simko <viliam.simko@gmail.com>
2015-12-08 19:59:15 +0100

    Improved tests for createScript function - better code coverage

--- tests/testthat/test-createScript.R
+++ tests/testthat/test-createScript.R
@@ -2,21 +2,31 @@ context("R scripts as UDF in EXASOL")
 
 # Added by Viliam Simko
 test_that("Creating a script without ODBC connection", {
-  testscript <- exa.createScript(
-    NULL, mockOnly = TRUE, # no connection inside unit-tests
-    "test.mymean", # script name
-    function(data) {
-      data$next_row(NA); # read all values from this group into a single vector
-      data$emit(data$groupid[[1]], mean(data$val))
-    },
-    inArgs = c( "groupid INT", "val DOUBLE" ),
-    outArgs = c( "groupid INT", "mean DOUBLE" )
-  )
 
-  expect_true(is.function(testscript))
-  sql <- testscript("groupid", "val", table = "test.twogroups" ,
-                    groupBy = "groupid", returnSQL = TRUE)
-  expect_equal(sql,
-    paste("(SELECT * FROM (SELECT test.mymean(groupid, val)",
-          "FROM test.twogroups GROUP BY groupid))") )
+  for (outType in c(EMITS, SET, SCALAR, RETURNS)) {
+    testscript <- exa.createScript(
+      NULL, mockOnly = TRUE, # no connection inside unit-tests
+      "test.mymean",
+      function(data) {},
+      initCode = function() {},
+      cleanCode = function() {},
+      inArgs = c( "groupid INT", "val DOUBLE" ),
+      outArgs = c( "groupid INT", "mean DOUBLE" ),
+      outType = outType
+    )
+
+    expect_true(is.function(testscript))
+    sql <- testscript("groupid", "val", table = "test.twogroups" ,
+                      groupBy = "groupid", returnSQL = TRUE)
+    expect_equal(sql,
+                 paste("(SELECT * FROM (SELECT test.mymean(groupid, val)",
+                       "FROM test.twogroups GROUP BY groupid))") )
+  }
+})
+
+# Added by Viliam Simko
+test_that("No output arguments should fail", {
+  expect_error(exa.createScript(
+    NULL, "test.mymean"
+  ), regexp = "No output arguments")
 })
