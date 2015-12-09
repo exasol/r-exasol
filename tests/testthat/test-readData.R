60b0d0e4d56cf5282406cd837c1114f159a0bfd3
Viliam Simko <viliam.simko@gmail.com>
2015-12-09 18:29:14 +0100

    Added a test that tries to connect to a local EXASOL VM. This test is ignored on Travis.

--- tests/testthat/test-readData.R
+++ tests/testthat/test-readData.R
@@ -8,3 +8,20 @@ test_that("Testing error cases", {
   expect_error(exa.readData(NULL, NULL, server = "localhost:0"),
                regexp = "Could not connect")
 })
+
+# Added by Viliam Simko
+test_that("Expecting that EXASOL VM runs locally", {
+  skip_on_travis()
+  skip_on_appveyor()
+  skip_on_cran()
+
+  # we expect this DSN to be configured in odbc.ini
+  C <- odbcConnect("exasolution")
+  expect_equal(exa.readData(C, "select * from DUAL"),
+               data.frame(DUMMY = NA))
+
+  tables <- exa.readData(C, "SELECT * FROM EXA_ALL_TABLES")
+  expect_true(is.data.frame(tables))
+
+  odbcClose(C)
+})
