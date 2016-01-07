2a63eaf6e7e9f0aff110f7c8043b82eb3a5d9471
Marcel Boldt <boldt@live.de>
2016-01-07 18:47:17 +0100

    corrected DSN

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,4 +1,5 @@
-DBItest::make_context(exasol(), list(dsn="exasolution-uo2214lv2_64"))
+DBItest::make_context(exasol(), list(dsn="exasolution-uo2214lv1_64"))
 DBItest::test_getting_started()
 DBItest::test_driver(skip=c("constructor_strict"))
 DBItest::test_connection()
+
