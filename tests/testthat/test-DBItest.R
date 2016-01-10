361ee540242754bc2c8b2354c35597f50bb4a00a
Marcel Boldt <boldt@live.de>
2016-01-10 20:53:59 +0100

    changed DBItest repo to marcelboldt/DBItest

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,6 +1,5 @@
-DBItest::make_context(exasol(), list(dsn="exasolution-uo2214lv1_64", schema="PUB864"),
-#DBItest::make_context(exasol(), list(dsn="EXASolo", schema="TEST"),
-                         tweaks = list(dummy_table = "DUAL"))
+DBItest::make_context(exasol(), list(dsn="exasolution-uo2214lv1_64", schema="PUB864"), tweaks = list(dummy_table = "DUAL"))
+#DBItest::make_context(exasol(), list(dsn="EXASolo", schema="TEST"), tweaks = list(dummy_table = "DUAL"))
 DBItest::test_getting_started()
 DBItest::test_driver(skip=c("constructor_strict" # stays skipped for now
                             ))
