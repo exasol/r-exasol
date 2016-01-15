6d619d93745f462108cf492e2d393413bfa29494
Marcel Boldt <boldt@live.de>
2016-01-15 11:19:28 +0100

    fixed encoding issues

--- R/EXADBI.R
+++ R/EXADBI.R
@@ -1224,3 +1224,26 @@ EXAApply <- function(res, fun,simplify,...) {
   res2
 }
 
+#' Shows the EXASOL ODBC connection log.
+#' @param con An EXAConnection object
+#' @param view logical If true, the log is shown in R View, otherwise, a data.frame is returned.
+#' @return a data.frame, if View is FALSE.
+#' @export
+EXAConnectionLog <- function (con, view = TRUE) {
+
+  logItems <- function(l) {
+    time <- regmatches(l,gregexpr("^\\d\\d:\\d\\d:\\d\\d.\\d\\d\\d",l, perl=TRUE))
+    time <- substr(time,0,nchar(time))
+    num <- substr(regmatches(l,gregexpr("^.{12}\\t\\d+?\\t",l, perl=TRUE)),14,18)
+    text <- sub("^.{12}\\t\\d+?\\t", "",l)
+    data.frame(time=time,num=num, text=text)
+  }
+
+  lstr <- regmatches(con@init_connection_string,gregexpr("EXALOGFILE=\\S+?;",con@init_connection_string,perl=TRUE))[[1]]
+  lstr <- substring(lstr, 12, nchar(lstr)-1)
+  con <- file(lstr)
+  df <- logItems(readLines(con))
+  df$time <- strptime(df$time, format="%T")
+  if (view) View(df)
+  else return(df)
+}
--- examples/writeData.R
+++ examples/writeData.R
@@ -17,4 +17,7 @@ twogroups <- data.frame(group = rep(1:2, each = 10),
 odbcQuery(C, "CREATE SCHEMA test")
 odbcQuery(C, "CREATE TABLE test.twogroups (groupid INT, val DOUBLE)")
 exa.writeData(C, twogroups, tableName = "test.twogroups")
+
+# setting the write encoding manually (See also EXASOL User Manual, Appendix D. "Supported Encodings")
+exa.writeData(con, twogroups, "test.twogroups", encoding="1252")
 }
