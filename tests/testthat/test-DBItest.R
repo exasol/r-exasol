5d8e6d27025bc32b8d841e6ee7fdb58524a56455
Marcel Boldt <boldt@live.de>
2016-04-18 19:30:14 +0200

    added dplyr support

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -9,13 +9,42 @@ DBItest::test_getting_started()
 # stress_load_unload and stress_load_connect_unload are also skipped on travis
 DBItest::test_driver(skip = c("constructor_strict",
                               "stress_load_unload"))
-#DBItest::test_connection(skip = c("stress_load_connect_unload"))
+DBItest::test_connection(skip = c("stress_load_connect_unload"))
 
 DBItest::test_result(skip = c("stale_result_warning",
                               "fetch_premature_close",
                               "data_logical_int",
                               "data_logical_int_null_below",
-                              "data_logical_int_null_above"))
+                              "data_logical_int_null_above",
+# following tests are TODOs
+                              "data_64_bit",
+                              "data_64_bit_null_below",
+                              "data_64_bit_null_above",
+                              "data_character",
+                              "data_character_null_below",
+                              "data_character_null_above",
+                              "data_raw",
+                              "data_raw_null_below",
+                              "data_raw_null_above",
+                              "data_date",
+                              "data_date_null_below",
+                              "data_date_null_above",
+                              "data_time",
+                              "data_time_null_below",
+                              "data_time_null_above",
+                              "data_time_parens",
+                              "data_time_parens_null_below",
+                              "data_time_parens_null_above",
+                              "data_timestamp",
+                              "data_timestamp_null_below",
+                              "data_timestamp_null_above",
+                              "data_timestamp_utc",
+                              "data_timestamp_utc_null_below",
+                              "data_timestamp_utc_null_above",
+                              "data_timestamp_parens",
+                              "data_timestamp_parens_null_below",
+                              "data_timestamp_parens_null_above"
+                              ))
 
 #DBItest::test_sql()
 #DBItest::test_meta()
