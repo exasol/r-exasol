cf18a928042fe65c349af98b57199d2126ea1234
Marcel Boldt <boldt@live.de>
2016-05-01 13:28:59 +0200

    changed darwin ODBC driver

--- R/EXADBI.R
+++ R/EXADBI.R
@@ -108,7 +108,7 @@ exasol <- function(driver = NULL, silent = FALSE) {
       switch(
         Sys.info()['sysname'],
         'Darwin' = paste0(system.file(package = packageName()),
-                          '/odbc/lib/darwin/x86_64/libexaodbc-io418sys.dylib'
+                          '/odbc/lib/darwin/x86_64/libexaodbc-io352fw.dylib'
                           ),  # Mac OS
         'Linux' = paste0(system.file(package = packageName()),
                          '/odbc/lib/linux/x86_64/libexaodbc-uo2214lv1.so'
