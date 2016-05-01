3e833a8524835359c6fe9efaf9d583dc5f716bea
Marcel Boldt <boldt@live.de>
2016-05-01 12:31:20 +0200

    APPLE: 0x01010000

--- src/exasol.c
+++ src/exasol.c
@@ -338,7 +338,7 @@ SEXP asyncRODBCIOStart(SEXP slotA, SEXP hostA, SEXP portA) {
 #endif
 #else
 #ifdef __APPLE__
-    proxy_header.x = 0x01000000;
+    proxy_header.x = 0x01010000;
     proxy_header.y = 0x01000000;
 #else
     proxy_header.x = 1;
