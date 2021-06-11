b2f47ba7187fd9a3019fb4ec5a581999d235ff3c
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-06-11 12:45:36 +0200

    #41: changed code comment

--- inst/rstudio/connections/Exasol Connection.R
+++ inst/rstudio/connections/Exasol Connection.R
@@ -1,6 +1,6 @@
 library(exasol)
 
-# Connect to Exasol DB with an ODBC DSN
+# Connect to Exasol DB
 exaconn <- dbConnect(
   drv     = "exa",                  # EXAdriver object
   exahost = "192.168.56.103:8563",  # IP of database cluster
