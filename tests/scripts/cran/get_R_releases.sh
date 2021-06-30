6dc2422551f06dc78ae4af03d03fcce1b6d0e668
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-06-30 08:51:58 -0300

    Add more debug log

--- tests/scripts/cran/get_R_releases.sh
+++ tests/scripts/cran/get_R_releases.sh
@@ -3,7 +3,10 @@
 set -e
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-#$SCRIPT_DIR/install_cran_repos.sh
+
+echo "BEGIN get_R_releases.sh"
+$SCRIPT_DIR/install_cran_repos.sh
 
 R_RELEASES=$("$SCRIPT_DIR/parse_R_releases.sh")
+echo "BEGIN get_R_releases.sh"
 printf '%s\n' "${R_RELEASES[@]}" | jq -R . | jq -cs .
