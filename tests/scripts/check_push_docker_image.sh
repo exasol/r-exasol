2d5eab4973885bdc55662a2b23cca3e185c4c393
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-07-05 08:25:35 -0300

    Fixed check_push_docker_image.sh

--- tests/scripts/check_push_docker_image.sh
+++ tests/scripts/check_push_docker_image.sh
@@ -11,12 +11,11 @@ R_VERSION="$1"
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-COMPLETE_IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_image_complete_name.sh" "${R_VERSION}")"
 IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_image_name.sh")"
-TAG_NAME="$("$SCRIPT_DIR/construct_docker_tag_name.sh "${R_VERSION}")"
+TAG_NAME="$("$SCRIPT_DIR"/construct_docker_tag_name.sh "$R_VERSION")"
 
 
-AVAILABLE_ON_DOCKER_HUB="$("$SCRIPT_DIR/list_tags.sh" "${IMAGE_NAME} "${TAG_NAME}"")"
+AVAILABLE_ON_DOCKER_HUB="$("$SCRIPT_DIR/list_tags.sh" "$IMAGE_NAME" "$TAG_NAME")"
 
 if [[ -z $AVAILABLE_ON_DOCKER_HUB ]]; then
   $SCRIPT_DIR/push_docker_image.sh "$R_VERSION"
