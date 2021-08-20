272a64f5ffe82f7e292bfee0c4f262ec41869481
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-08-20 16:55:27 +0000

    Added winlibs.R

---
+++ tools/winlibs.R
@@ -0,0 +1,12 @@
+# Build against mingw-w64 build of openssl
+VERSION <- commandArgs(TRUE)
+if(!file.exists(sprintf("../windows/openssl-%s/include/openssl/ssl.h", VERSION))){
+  if(getRversion() < "3.3.0") setInternet2()
+  download.file(sprintf("https://github.com/rwinlib/openssl/archive/v%s.zip", VERSION),
+                "lib.zip", quiet = FALSE)
+  dir.create("../windows", showWarnings = TRUE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+} else {
+  warning("OpenSSL already installed")
+}
