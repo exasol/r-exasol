c8306e879b771971683f65485df9e2de0ed08b85
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-05 10:03:48 -0300

    Using RODBC

--- R/Viewer.R
+++ R/Viewer.R
@@ -1,3 +1,12 @@
+.exa_tables <- function(connection, schema = NULL, types = c("VIEW", "TABLE")) {
+  tables <- RODBC::sqlTables(connection)
+  tables <- tables[tables$TABLE_TYPE %in% types,]
+  if (!is.null(schema)) {
+    tables <- tables[tables$TABLE_SCHEM %in% c(schema),]
+  }
+  tables
+}
+
 .odbcListObjectTypes <- function(connection) {
   # slurp all the objects in the database so we can determine the correct
   # object hierarchy
@@ -5,65 +14,42 @@
   # all databases contain tables, at a minimum
   obj_types <- list(table = list(contains = "data"))
 
+  tables <- .exa_tables(connection)
+
   # see if we have views too
-  views_df <- exa.readData(connection, "SELECT * FROM EXA_ALL_VIEWS")
-  if (nrow(views_df) >0) {
+  table_types <- tolower(unique(tables$TABLE_TYPE))
+  if ("view" %in% table_types) {
     obj_types <- c(obj_types, list(view = list(contains = "data")))
   }
 
-  obj_types <- c(obj_types, list(db_function = list(contains = "data")))
-
   # assume we always have schemas!
   obj_types <- list(schema = list(contains = obj_types))
-
-  #
-  # # check for multiple catalogs
-  # catalogs <- string_values(connection_sql_tables(connection@ptr, "%", "", "", "")[["table_catalog"]])
-  # if (length(catalogs) > 0) {
-  #   obj_types <- list(catalog = list(contains = obj_types))
-  # }
 
   obj_types
 }
 
 .list_table_names <- function(connection, schema, name = NULL) {
-  q <- paste0("SELECT TABLE_NAME FROM EXA_ALL_TABLES WHERE TABLE_SCHEMA=", processIDs(schema, quotes="'"))
+  tables <- .exa_tables(connection, schema, c("TABLE"))
   if (!is.null(name)) {
-    q <- paste0(q, " AND TABLE_NAME=", processIDs(name, quotes = "'"))
+    tables <- tables[tables$TABLE_NAME %in% c(name),]
   }
-  tables_df <- exa.readData(connection, q)
 
   data.frame(
-    name = tables_df$TABLE_NAME,
-    type = rep("table", nrow(tables_df)),
+    name = tables$TABLE_NAME,
+    type = rep("table", length(tables$TABLE_NAME)),
     stringsAsFactors = FALSE
   )
 }
 
 .list_view_names <- function(connection, schema, name = NULL) {
-  q <- paste0("SELECT VIEW_NAME FROM EXA_ALL_VIEWS WHERE VIEW_SCHEMA=", processIDs(schema, quotes="'"))
+  views <- .exa_tables(connection, schema, c("VIEW"))
   if (!is.null(name)) {
-    q <- paste0(q, " AND VIEW_NAME=", processIDs(name, quotes = "'"))
+    views <- views[views$TABLE_NAME %in% c(name),]
   }
-  views_df <- exa.readData(connection, q)
 
   data.frame(
-    name = views_df$VIEW_NAME,
-    type = rep("view", nrow(views_df)),
-    stringsAsFactors = FALSE
-  )
-}
-
-.list_function_names <- function(connection, schema, name = NULL) {
-  q <- paste0("SELECT FUNCTION_NAME FROM EXA_ALL_FUNCTIONS WHERE FUNCTION_SCHEMA=", processIDs(schema, quotes="'"))
-  if (!is.null(name)) {
-    q <- paste0(q, " AND FUNCTION_NAME=", processIDs(name, quotes = "'"))
-  }
-  views_df <- exa.readData(connection, q)
-
-  data.frame(
-    name = c("TestFunc"),
-    type = c("db_function"), #rep("db_function", nrow(views_df)),
+    name = views$VIEW_NAME,
+    type = rep("view", length(views$VIEW_NAME)),
     stringsAsFactors = FALSE
   )
 }
@@ -72,14 +58,14 @@ odbcListObjects <- function(connection, catalog = NULL, schema = NULL, name = NU
 
   res <- data.frame()
   # if no schema was supplied, return a list of schema
-  warning(paste0("odbcListObjects called with(schema=", schema, " name=", name, " type=", type, ")\n"))
   if (is.null(schema)) {
-    schemas_df <- exa.readData(connection, "SELECT SCHEMA_NAME FROM EXA_ALL_SCHEMAS")
-    if (nrow(schemas_df) > 0) {
+    tables <- .exa_tables(connection)
+    schemas <- unique(tables$TABLE_SCHEM)
+    if (length(schemas) > 0) {
       res <-
         data.frame(
-          name = schemas_df$SCHEMA_NAME,
-          type = rep("schema", times = nrow(schemas_df)),
+          name = schemas,
+          type = rep("schema", times = length(schemas)),
           stringsAsFactors = FALSE
       )
     }
@@ -87,17 +73,13 @@ odbcListObjects <- function(connection, catalog = NULL, schema = NULL, name = NU
   else if (is.null(type)){
     tables_df <- .list_table_names(connection, schema, name)
     views_df <- .list_view_names(connection, schema, name)
-    functions_df <- .list_function_names(connection, schema, name)
-    res <- rbind(tables_df, views_df, functions_df)
+    res <- rbind(tables_df, views_df)
   }
   else if (tolower(type) == "table") {
     res <- .list_table_names(connection, schema, name)
   }
   else if (tolower(type) == "view") {
     res <- .list_view_names(connection, schema, name)
-  }
-  else if (tolower(type) == "db_function") {
-    res <- .list_function_names(connection, schema, name)
   }
   res
 }
@@ -137,26 +119,14 @@ odbcListObjects <- function(connection, catalog = NULL, schema = NULL, name = NU
   if (is.null(schema)) {
     stop("Schema must not be null")
   }
-  .validateObjectName(table, view)
+  name <- .validateObjectName(table, view)
 
-  if(!is.null(table)) {
-    q <-  paste0("SELECT COLUMN_NAME, COLUMN_TYPE FROM EXA_ALL_COLUMNS WHERE COLUMN_SCHEMA=",
-                 processIDs(schema, quotes="'") , " AND COLUMN_TABLE=",  processIDs(table, quotes="'"),
-                 " AND COLUMN_OBJECT_TYPE='TABLE'")
-    columns_df <- exa.readData(connection, q)
-    res <- data.frame(
-      name = columns_df$COLUMN_NAME,
-      type = columns_df$COLUMN_TYPE,
-      stringsAsFactors = FALSE)
-  } else {
-    columns_df <- exa.readData(connection, paste0("SELECT COLUMN_NAME, COLUMN_TYPE FROM EXA_ALL_COLUMNS WHERE COLUMN_SCHEMA=",
-                                                  processIDs(schema, quotes="'") , " AND COLUMN_TABLE=",  processIDs(view, quotes="'"),
-                                                  " AND COLUMN_OBJECT_TYPE='VIEW'"))
-    res <- data.frame(
-      name = columns_df$COLUMN_NAME,
-      type = columns_df$COLUMN_TYPE,
-      stringsAsFactors = FALSE)
-  }
+  columns <- RODBC::sqlColumns(channel = connection, sqtable = name, schema = schema)
+
+  res <- data.frame(
+    name = columns$COLUMN_NAME,
+    type = columns$TYPE_NAME,
+    stringsAsFactors = FALSE)
   res
 }
 
@@ -169,9 +139,6 @@ odbcListObjects <- function(connection, catalog = NULL, schema = NULL, name = NU
   if (is.null(schema)) {
     stop("Schema not defined.")
   }
-  #res <- dbSendQuery(connection, statement = paste0("SELECT * FROM ", schema, ".", name), default_fetch_rec = rowLimit)
-  #retVal <- dbFetch(res, rowLimit)
-  #dbClearResult(res)
   retVal = exa.readData(connection, paste0("SELECT * FROM ", schema, ".", name, " LIMIT ", rowLimit))
   retVal
 }
