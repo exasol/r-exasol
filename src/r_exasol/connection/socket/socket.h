35e2b548d71a604c46e83b086e63171d664a8947
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-12 07:05:16 -0300

    Added documentation to socket.h

--- src/r_exasol/connection/socket/socket.h
+++ src/r_exasol/connection/socket/socket.h
@@ -13,9 +13,33 @@ namespace exa {
     class Socket {
     public:
         virtual ~Socket() = default;
+        /**
+         * Receive a buffer from the socket. The method blocks until the buffer is received.
+         * @param buf The pointer of the buffer to receive the data.
+         * @param len The size of the buffer.
+         * @return -1 if an error occurred. Otherwise the size of the buffer which was received.
+         */
         virtual ssize_t recv(void *buf, size_t len) = 0;
+        /**
+         * Send data to socket. The method is none-blocking.
+         * @param buf Pointer to buffer to send.
+         * @param len Length of the buffer to send.
+         * @return -1 if an error occurred. Otherwise the length of the buffer which was sent.
+         */
         virtual ssize_t send(const void *buf, size_t len) = 0;
+        /**
+         * Closes socket for sending.
+         * All ongoing send operations will be finished, but future send operations on the socket
+         * are not allowed.
+         * @see https://www.baeldung.com/cs/sockets-close-vs-shutdown#shutdown
+         */
         virtual void shutdownWr() = 0;
+        /**
+         * Closes socket for both sending/receiving.
+         * All ongoing send/receive operations will be finished, but future send/receive operations on the socket
+         * are not allowed.
+         * @see https://www.baeldung.com/cs/sockets-close-vs-shutdown#shutdown
+         */
         virtual void shutdownRdWr() = 0;
     };
 }
