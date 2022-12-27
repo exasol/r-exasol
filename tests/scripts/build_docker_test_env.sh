993c23786001996b930d65ec5dd935f598964b94
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-12-27 09:30:41 -0300

    forward GITHUB_TOKEN to Docker build

--- tests/scripts/build_docker_test_env.sh
+++ tests/scripts/build_docker_test_env.sh
@@ -14,4 +14,4 @@ R_VERSION=$(echo "$1" | cut -f 2 -d "/")
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_image_complete_name.sh" "${COMBINED_R_VERSION}")"
-docker build -t "$IMAGE_NAME" --cache-from "$IMAGE_NAME" --build-arg R_VERSION="$R_VERSION" --build-arg CRAN_REPO="$CRAN_REPO" "$SCRIPT_DIR/../"
+docker build -t "$IMAGE_NAME" --cache-from "$IMAGE_NAME" --build-arg GH_TOKEN="$GITHUB_TOKEN" --build-arg R_VERSION="$R_VERSION" --build-arg CRAN_REPO="$CRAN_REPO" "$SCRIPT_DIR/../"
