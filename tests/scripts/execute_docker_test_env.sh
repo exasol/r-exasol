970fccf7e6a68794f273ef9f133027fdd16c4aca
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-03-31 07:04:45 -0300

    Fixes from review

--- tests/scripts/execute_docker_test_env.sh
+++ tests/scripts/execute_docker_test_env.sh
@@ -19,5 +19,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 docker container rm -f -v r_test || true
 IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_image_complete_name.sh" "${COMBINED_R_VERSION}")"
 PROJ_DIR="$(realpath "$SCRIPT_DIR/../..")"
-CERTIFICATE_PATH=${R_TESTS_CERTIFICATE_PATH:-/tmp/certificate/}
+CERTIFICATE_PATH=${R_TESTS_CERTIFICATE_PATH:-/tmp/certificate/} #Get path where certificate of the docker-db is stored. We then mount this path under "/certificate" in the test container, which is the location the CI tests expect it to be placed.
 docker run  --network host --name r_test -v "$PROJ_DIR":"$PROJ_DIR" -v "$CERTIFICATE_PATH":"/certificate/" --add-host exasol-test-database:127.0.0.1 -t "$IMAGE_NAME" "$PROJ_DIR" "$VARIANT"
