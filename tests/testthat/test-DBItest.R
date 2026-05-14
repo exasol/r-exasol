046c135a16d869c7d96f821199a4693bd6455b70
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-05-14 09:37:00 -0300

    Fixed test-DBItest.R

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,7 +1,13 @@
 if (Sys.getenv("HAS_LOCAL_EXASOL_TEST_DB") == "true") {
-  DBItest::make_context(exasol_driver(),
-                        list(exahost = "exasol-test-database:8888", uid="sys", pwd="exasol", sslcertificate="/certificate/rootCA.crt"),
-                        tweaks = list(dummy_table = "DUAL"))
+  DBItest::make_context(
+    exasol_driver(),
+    list(
+      exahost        = Sys.getenv("EXAHOST",           "exasol-test-database:8888"),
+      uid            = Sys.getenv("EXAUID",            "sys"),
+      pwd            = Sys.getenv("EXAPWD",            "exasol"),
+      sslcertificate = Sys.getenv("EXASSLCERTIFICATE", "/certificate/rootCA.crt")
+    ),
+    tweaks = list(dummy_table = "DUAL"))
 
   source("run-dbi-tests.R")
 }
